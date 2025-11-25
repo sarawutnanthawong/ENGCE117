7a6323bc95c42beb7d454dc9d8157a9c882ac208
Doinet <153589737+sarawutnanthawong@users.noreply.github.com>
2025-11-25 20:51:58 +0700

    Add files via upload

---
+++ homework1_1.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+    int N, i;
+    
+    
+    if (scanf("%d", &N) != 1) {
+        return 1; 
+    }
+    
+    if (N <= 0) {
+        printf("Error");
+    } else {
+        for (i = 0 ; i < N ; i++);
+            printf("Heelo loop!\n");
+    }
+    
+
+    return 0;
+}
