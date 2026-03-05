fd64ddb2dae6d76586598e54893766e8a5c4f028
Doinet <153589737+sarawutnanthawong@users.noreply.github.com>
2026-03-06 01:25:58 +0700

    Add files via upload

---
+++ lab117_10_2.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <cstdio>
+
+using namespace std;
+
+void TowerHanoi(int m, int i, int j);
+
+int main() {
+    TowerHanoi(3, 1, 3);
+    return 0;
+}
+
+void TowerHanoi(int m, int i, int j) {
+    if (m > 0) {
+        int k = 6 - i - j;
+        TowerHanoi(m - 1, i, k);
+        printf("Disc %d from %d to %d\n", m, i, j);
+        TowerHanoi(m - 1, k, j);
+    }
+}
